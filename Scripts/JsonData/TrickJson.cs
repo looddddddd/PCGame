7ae8b4d1729f13583707ef50e17f385cd30fadf8
looddddddd <1779734623@qq.com>
2017-03-15 19:43:28 +0800

    add

--- Scripts/JsonData/TrickJson.cs
+++ Scripts/JsonData/TrickJson.cs
@@ -2,9 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 /// <summary>
-/// 绝招数据模型--5
+/// 绝招数据模型--最多携带5个技能--系统技能待定
 /// </summary>
 public class TrickJson 
 {
-
+    public int id;
+    public int orderId;
+    public int type;
+    public string name;
+    public int level;
+    public int maxLevel;
+    public string picUrl;
+    public int att;
+    public int exp;
+    public int nextExp;
+    // public TrickView view;
 }
