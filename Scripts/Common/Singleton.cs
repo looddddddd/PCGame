cdde034d8c277f326c40d2f42af0bec821895815
looddddddd <1779734623@qq.com>
2017-04-12 14:50:30 +0800

    Update Singleton.cs

--- Scripts/Common/Singleton.cs
+++ Scripts/Common/Singleton.cs
@@ -1,6 +1,10 @@
 ﻿using UnityEngine;
 using System.Collections;
-
+/// <summary>
+///  单例模式及常见写法分析
+/// http://blog.csdn.net/jiankunking/article/details/50867050
+/// </summary>
+/// <typeparam name="T"></typeparam>
 public class Singleton<T> where T:new()
 {
     private static T instance;
